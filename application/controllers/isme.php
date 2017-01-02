e3f3be9383538b826e380f13408ab423fe10abc2
John.Go <gogwangbok@gmail.com>
2017-01-02 11:48:18 +0900

    First commit

--- application/controllers/isme.php
+++ application/controllers/isme.php
@@ -5,27 +5,27 @@ class Isme extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		
+
 		// Load models
 		// $this->load->model('user_model','user');
-	}	
-	
+	}
+
 	function index()
-	{		
+	{
 		if($this->session->userdata('state'))
 		{
 			$public['page'] = 'isme';
 
 			$this->load->view('head');
-			$this->load->view('nav');		
-			
+			$this->load->view('nav');
+
 			$this->load->view('isme_map');
 			$this->load->view('js',$public);
 			$this->load->view('footer');
 		}
 		else
 		{
-			redirect('/login');	
+			redirect('/login');
 		}
 	}
 
@@ -43,6 +43,5 @@ class Isme extends CI_Controller {
 	// 		$json['result'] = false;
 	// 	}
 
-	// 	$this->output->set_content_type('application/json')->set_output(json_encode($json));
-	// }
+
 }
